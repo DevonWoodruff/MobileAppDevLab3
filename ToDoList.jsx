334579d278be28f2fe3171a3d1610d9b53eeeb0f
DevonWoodruff <119452528+DevonWoodruff@users.noreply.github.com>
2023-11-02 21:28:14 -0600

    Lab 3 complete

---
+++ ToDoList.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {
+    Pressable,
+    View,
+    Text,
+    StyleSheet,
+    ScrollView  } from 'react-native';
+
+function ToDoList({ tasks }) {
+    return (
+<ScrollView>
+        <Pressable>
+          <View style={[styles.task]}>
+            {tasks.map((task, index) => (
+            <Text key={index} style={styles.taskText}>{task}</Text>
+            ))}
+          </View>
+        </Pressable>
+      </ScrollView>
+    )
+};
+
+const styles = StyleSheet.create({
+    task: {
+      padding: 10,
+      borderBottomWidth: 1,
+      borderColor: '#ccc',
+    },
+    completed: {
+      backgroundColor: '#e0e0e0',
+    },
+    taskText: {
+      fontSize: 16,
+    },
+    form: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginHorizontal: 20,
+      marginTop: 20,
+    },
+    input: {
+      flex: 1,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      marginRight: 10,
+    },
+  });
+
+export default ToDoList;
